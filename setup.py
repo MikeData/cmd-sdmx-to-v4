cb8d9887e115c7114305a609fb439728a771ed27
mikeAdams <mikelovesbooks@gmail.com>
2018-04-05 14:50:33 +0100

    resturctured for git install

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+from distutils.core import setup
+setup(
+  name = 'SDMXtoV4',
+  packages = ['SDMXtoV4'],
+  version = '0.1',
+  description = 'Converts SDMX datasets to V4 load files. ',
+  author = 'Michael Adams',
+  author_email = 'michael.adams@ons.gov.uk',
+  url = 'https://github.com/ONS-OpenData/SDMXtoV4',
+  download_url = 'https://github.com/ONS-OpenData/SDMXtoV4/archive/0.1.tar.gz',
+  keywords = ['extraction', 'SDMX', 'data'],
+  classifiers = [],
+)
